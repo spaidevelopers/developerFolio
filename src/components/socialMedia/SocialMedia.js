df3de535be87a725f81ebdea11e297f888f7b298
Tousif Khan <spaidevelopers@gmail.com>
2023-10-13 18:12:22 +0500

    fixes

--- src/components/socialMedia/SocialMedia.js
+++ src/components/socialMedia/SocialMedia.js
@@ -127,6 +127,12 @@ export default function socialMedia() {
           <span></span>
         </a>
       ) : null}
+
+
+      <div class="trustpilot-widget" data-locale="en-US" data-template-id="56278e9abfbbba0bdcd568bc" data-businessunit-id="6527f2278860e2ce4e38998f" data-style-height="52px" data-style-width="100%">
+        <a href="https://www.trustpilot.com/review/tousif.me" target="_blank" rel="noopener">Trustpilot</a>
+      </div>
+
     </div>
   );
 }
