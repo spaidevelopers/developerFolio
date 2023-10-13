63c12d12619774db0de5bc3d0f1bd78bc6a94be3
Tousif Khan <spaidevelopers@gmail.com>
2023-10-13 18:18:29 +0500

    fixes

--- src/containers/contact/Contact.js
+++ src/containers/contact/Contact.js
@@ -53,6 +53,10 @@ export default function Contact() {
               <br />
               <SocialMedia />
             </div>
+            <div className="trustpilot-widget" data-locale="en-US" data-template-id="56278e9abfbbba0bdcd568bc"
+                 data-businessunit-id="6527f2278860e2ce4e38998f" data-style-height="52px" data-style-width="100%">
+              <a href="https://www.trustpilot.com/review/tousif.me" target="_blank" rel="noopener">Trustpilot</a>
+            </div>
           </div>
           <div className="contact-image-div">
             {/*{illustration.animated ? (*/}
